1377eaeeac27b1227dca5fcdf63a3fba99d5251e
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-05-01 20:58:06 -0300

    Devmind commit in 2018-05-01 20:58:05

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var payment = require("./routes/payment");
 
 var app = express();
 
-// view engine setupp
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
